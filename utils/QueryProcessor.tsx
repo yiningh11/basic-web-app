9bcd9207886c71dadec1c72daaf0355021a1b783
Yining He <yininghe@andrew.cmu.edu>
2025-02-25 10:14:39 -0500

    added new questions

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -22,12 +22,31 @@ export default function QueryProcessor(query: string): string {
     return (num1 + num2).toString();
   }
 
+  const multiplicationMatch = query.match(/what is (\d+) multiplied by (\d+)\?/i);
+  if (multiplicationMatch) {
+    const num1 = parseInt(multiplicationMatch[1], 10);
+    const num2 = parseInt(multiplicationMatch[2], 10);
+    return (num1 * num2).toString();
+  }
+
+
   const largestMatch = query.match(/which of the following numbers is the largest: ([\d, ]+)\?/i);
   if (largestMatch) {
     const numbers = largestMatch[1].split(',').map(num => parseInt(num.trim(), 10));
     return Math.max(...numbers).toString();
   }
   
+  const squareCubeMatch = query.match(/which of the following numbers is both a square and a cube: ([\d, ]+)\?/i);
+  if (squareCubeMatch) {
+    const numbers = squareCubeMatch[1].split(',').map(num => parseInt(num.trim(), 10));
+    const isSquareAndCube = (num: number) => {
+      const root = Math.round(Math.pow(num, 1 / 6));
+      return root ** 2 * root ** 3 === num;
+    };
+    const result = numbers.find(isSquareAndCube);
+    return result !== undefined ? result.toString() : "None";
+  }
 
+  
   return "";
 }
